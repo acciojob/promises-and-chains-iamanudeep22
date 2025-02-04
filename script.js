3ca53e0c67aeecfbc7d513669c56d5c2cf70b7dc
Anudeep KN <116871481+iamanudeep22@users.noreply.github.com>
2025-02-05 01:16:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function validate(){
+    const age = document.getElementById("age");
+    const name = document.getElementById("name");
+    const btn = document.getElementById("btn");
+	if(age.value && name.value){
+        let promise =  new Promise((resolve, reject) => {
+            if(age.value >= 18){
+                setTimeout(() => resolve(`Welcome, ${name.value}. You can vote.`), 4000);
+            }else{
+                setTimeout( () => reject(`Oh sorry ${name.value}. You aren't old enough.`), 4000 );
+            }
+        })
+        promise.then((message) => {
+            alert(message);
+        })
+        .catch((error) => {
+            alert(error)
+        })
+	}else{
+        alert("Please enter valid details.");
+    }
+}
